75be8f8313d11d7cf11bf5da81b62cb6fbab6324
MaxAlieksieiev <corp.workMA@gmail.com>
2020-07-02 11:05:56 +0300

    add header slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.header__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        arrows: false,
+    });
+  });
+          
